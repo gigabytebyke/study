76993a341ba8c2056ae9dc8993f664bd1eb76c5e
Giga Tukhashvili <unknowntukhashvili@gmail.com>
2020-09-28 12:37:59 +0400

    pizza and countdown tasks refactor

---
+++ pizza.js
@@ -0,0 +1,18 @@
+const pizzas = [
+    {
+        name: "პეპერონი",
+        price: 20
+    },
+    {
+        name: "მარგარიტა",
+        price: 15
+    },
+    {
+        name: "კომბინირებული",
+        price: 22
+    }
+];
+const randomElement = array[Math.floor(Math.random() * pizzas.length)]; 
+console.log("თქვენი პიცა (random, pizzas[random]) მზადდება");
+
+ 
--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-console.log(5);
-console.log(6);
-let x = 5;
+//4 steps to create new year's countdown timer:
+//Set a valid end date
+const deadline = 'December 31 2020 23:59:59';
+//Calculate the time remaining //Convert the time to a usable format.
+function getTimeRemaining(endtime){
+    console.clear();
+    const total = Date.parse(endtime) - Date.parse(new Date());//to find the remaining time until the deadline (in milli seconds by function date.parse())
+    const seconds = Math.floor( (total/1000) % 60 );  //we don’t want all of the seconds, just those remaining after the minutes have been counted:  (t/1000) % 60)
+    const minutes = Math.floor( (total/1000/60) % 60 );  //same for minutes ..we need minutes after hours
+    const hours = Math.floor( (total/(1000*60*60)) % 24 );   // we use the function math.floor 
+    const days = Math.floor( total/(1000*60*60*24) );
+    //Output the Clock Data as a Reusable Object
+    console.log(days + "d " + hours + "h " + minutes + "m " + seconds + "s");
+}
+
+setInterval(getTimeRemaining, 1000, deadline);
+ 
+
+
